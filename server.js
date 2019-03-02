518d21c7d5b0109f3cffa7b12753fe6ee75bb4af
pl <7350206@pl.com>
2019-03-02 22:48:23 +0300

    handling delete with some refactoring

--- server.js
+++ server.js
@@ -25,13 +25,10 @@ app.get('/api/posts/:year/:month', (req, res) => {
 })
 
 app.get('/api/posts/:id', (req, res) => {
-  // !! req.params.id returns a String
+  // !! req.params.id returns a String, so make it a Int
   const post =  posts.find(p => p.id === parseInt(req.params.id))
-  if(!post){
-    res
-      .status(404)
-      .send(`post ${req.params.id} not found`)
-  }
+  if(!post) return res.status(404).send(`post ${req.params.id} not found`)
+  // if exist
   res.send(post)
 })
 
@@ -65,28 +62,39 @@ app.post('/api/posts', (req, res) => {
 app.put('/api/posts/:id', (req, res) => {
   // 1)check if post exists, if no - return 404 and exit
   const post = posts.find(p => p.id === parseInt(req.params.id))
-  if(!post){
+  /* if(!post){
     res.status(404).send('post not found')
     return
-  }
+  } */
+  if (!post) return res.status(404).send('Post for update was not found')
 
   // 2) validate, if no - return 400 and exit
   // reuse external validation
   // const result = validatePost(req.body)
   // ------- use object destruct (validatePost returns {error, value})
   const {error} = validatePost(req.body) //result.error
-  if(error){
-    res
-      .status(400)
-      .send(error.details[0].message)
-      return
-  }
+  if(error) return res.status(400).send(error.details[0].message)
 
   // 3) update and show updated on client
   post.name = req.body.name
   res.send(posts)
-
 } )
+
+
+// DELETE REQ
+app.delete('/api/posts/:id', (req, res) => {
+  // look up post, if !exists - return 404 and exit
+  const post = posts.find(p => p.id === parseInt(req.params.id))
+  if (!post) return res.status(404).send('post for deleting not found')
+
+  // delete
+  const index = posts.indexOf(post)
+  posts.splice(index, 1)  //onplace, maybe slice is better
+
+  // show others on client
+  res.send(posts)
+})
+
 
 /**
  * try external validation
@@ -101,6 +109,7 @@ function validatePost(post){
   }
   return Joi.validate(post, schema)
 }
+
 
 
 
