944b06e9145353ed0743dfe4522bca1d25322c0f
pl <7350206@pl.com>
2019-03-02 19:58:40 +0300

    handling get

--- server.js
+++ server.js
@@ -1,10 +1,35 @@
 const express = require('express')
 const app = express()
 
+const posts = [
+  {id:1, name: 'post1'},
+  {id:2, name: 'post2'},
+  {id:3, name: 'post3'},
+]
+
 app.get('/', (req, res) => {
   res
     .status(200)
     .send('hi')
+})
+
+app.get('/api/posts', (req, res) => {
+  res.send(posts)
+})
+
+app.get('/api/posts/:year/:month', (req, res) => {
+  res.send(req.params) //{"year":"2023","month":"2"}
+})
+
+app.get('/api/posts/:id', (req, res) => {
+  // !! req.params.id returns a String
+  const post =  posts.find(p => p.id === parseInt(req.params.id))
+  if(!post){
+    res
+      .status(404)
+      .send(`post ${req.params.id} not found`)
+  }
+  res.send(post)
 })
 
 
